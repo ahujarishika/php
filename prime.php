821fedf6f6a557f4c8a2479f4f7950bee321f106
Rishika Ahuja <58805820+ahujarishika@users.noreply.github.com>
2020-07-15 23:20:44 +0530

    Create prime.php

---
+++ prime.php
@@ -0,0 +1,27 @@
+
+ <?php		
+ $a=$_POST["lower"];
+ $b=$_POST["upper"];
+	
+if(isset($_POST["check"]))
+{
+$a=$_POST["lower"];
+$b=$_POST["upper"];
+$flag=0;
+for($a;$a<$b;$a++)
+{
+for($j=2;$j<$a;$j++)
+{
+if($a%$j==0)
+{
+$flag=1;
+}
+}
+if($flag==0)
+{
+echo " ".$a." ";
+}
+$flag=0;
+}
+}
+?>
